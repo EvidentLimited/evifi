9bfd230452b38055973779f0ddd8a457f78f14c6
devops-evident <devops@evident.capital>
2023-10-20 00:46:01 +0530

    fix: Added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes.js';
 import htmlExpress from 'html-express-js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 4000;
@@ -9,6 +10,7 @@ const PORT = 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
+app.use(cors());
 
 // set up engine
 app.engine(
