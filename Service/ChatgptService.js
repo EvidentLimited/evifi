b6cd206517b7bd3b22ea9fa5864cd3a10e63c538
devops-evident <devops@evident.capital>
2023-11-10 19:53:33 +0530

    fix: prompt

--- Service/ChatgptService.js
+++ Service/ChatgptService.js
@@ -19,7 +19,7 @@ Create "Your 5 Step Blueprint with EVIDENT", a structured guide with 5 bullet po
 5. Regular Updates: Continuously inform clients about their investment.
 Next, provide 3 examples that illustrate the potential of this tokenization, appealing to high-net-worth individuals and institutional investors. These examples should be concise and engaging.
 Conclude with a section "What you can do with the tokenized ${prompt}", comprising three categories: Unprecedented Liquidity, Unique Diversification, and Broad Appeal. Each category should be explained in a clear, concise sentence.
-The response should be an HTML code, with properly spaced and left-aligned bullet points, and devoid of jargon, buzzwords, cliches, complex metaphors, unnecessary adjectives, or humor. The aim is to pique interest in tokenization with EVIDENT and encourage clients to book a demo. Don't add extra quotation marks and only use numbered list. Make internal sections justified alignment and ensure font-size is consistent`;
+The response should be an HTML code, with properly spaced and left-aligned bullet points, and devoid of jargon, buzzwords, cliches, complex metaphors, unnecessary adjectives, or humor. The aim is to pique interest in tokenization with EVIDENT and encourage clients to book a demo. Don't add extra quotation marks and only use numbered list. Make internal sections justified alignment and ensure font-size is consistent and there should not be a starting and ending with quotation marks`;
 
       const data = await this.openai.chat.completions.create({
         messages: [{ role: 'user', content: responseFormat }],
