c86b68652ef9fd3765bda0d1778dd5cd5df45029
devops-evident <devops@evident.capital>
2023-11-10 19:58:33 +0530

    fix: prompt

--- Service/ChatgptService.js
+++ Service/ChatgptService.js
@@ -11,14 +11,14 @@ class ChatgptService {
   async askChatgpt(prompt) {
     try {
       const responseFormat = `Your task: As a high-level member of the EVIDENT Investment Management team, akin to Andrew Huberman in personality and tone, you are to simplify complex topics for your asset management clients. Provide an overview and a tokenization strategy for an asset they wish to tokenize and distribute to investors on the EVIDENT platform.
-Create "Your 5 Step Blueprint with EVIDENT", a structured guide with 5 bullet points to walk clients through the tokenization of the ${prompt}. The steps are:
+Create "Your 5 Step Blueprint with EVIDENT", a structured guide with 5 bullet points in justified alignment to walk clients through the tokenization of the ${prompt}. The steps are:
 1. Digital Creation: Convert the asset into a digital token.
 2. Value Confirmation: Establish the asset's market value.
 3. Legal Checkpoint: Ensure compliance with legal standards.
 4. EVIDENT Inclusion: Incorporate the tokenized asset into the EVIDENT platform.
 5. Regular Updates: Continuously inform clients about their investment.
-Next, provide 3 examples that illustrate the potential of this tokenization, appealing to high-net-worth individuals and institutional investors. These examples should be concise and engaging.
-Conclude with a section "What you can do with the tokenized ${prompt}", comprising three categories: Unprecedented Liquidity, Unique Diversification, and Broad Appeal. Each category should be explained in a clear, concise sentence.
+Next, provide 3 examples that illustrate the potential of this tokenization in justified alignment, appealing to high-net-worth individuals and institutional investors. These examples should be concise and engaging.
+Conclude with a section "What you can do with the tokenized ${prompt}?", comprising three categories and answer it in bullet points: Unprecedented Liquidity, Unique Diversification, and Broad Appeal. Each category should be explained in a clear, concise sentence.
 The response should be an HTML code, with properly spaced and left-aligned bullet points, and devoid of jargon, buzzwords, cliches, complex metaphors, unnecessary adjectives, or humor. The aim is to pique interest in tokenization with EVIDENT and encourage clients to book a demo. Don't add extra quotation marks and only use numbered list. Make internal sections justified alignment and ensure font-size is consistent and there should not be a starting and ending with quotation marks`;
 
       const data = await this.openai.chat.completions.create({
